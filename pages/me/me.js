e6976057823fb490ba5a4e96ef92f4c873d550fd
tianduo123 <954026506@qq.com>
2019-07-23 17:37:59 +0800

    2019/7/23

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,66 +1,94 @@
 // pages/me/me.js
+let app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    avatarUrl: '/icon/avatar.svg',
+    nickName: '点击登录'
+  },
+  getUserInfo(e) {
+    console.log(e)
+    if (e.detail.userInfo) {
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        nickName: e.detail.userInfo.nickName
+      })
+    }
+  },
+  //去订单页
+  toOrder(e){
+    if(app.globalData.userInfo){
+      console.log(e)
+      wx.navigateTo({
+        url: `../order/order?status=${e.currentTarget.dataset.status}`,
+      })
+    }else{
+      console.log('用户未授权')
+    }
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        avatarUrl: app.globalData.userInfo.avatarUrl,
+        nickName: app.globalData.userInfo.nickName
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
